958588922ae817bf3744eb88a92dc6672d0da379
Muzafer <40720221@live.napier.ac.uk>
2025-09-29 17:55:36 +0100

    Adding comment

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,5 +1,5 @@
 package com.napier.sem;
-
+//HEll
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
